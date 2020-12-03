11c53d63b39d4e1c1f068f270b80e6b22e38ef52
Mira <m13hall@gmail.com>
2020-12-02 21:56:54 -0800

    day 3

--- src/main/kotlin/Day3.kt
+++ src/main/kotlin/Day3.kt
@@ -1,5 +1,143 @@
-class Day3 {
-    fun doIt(){
-        println("doin it")
+/*
+ * --- Day 3: Toboggan Trajectory ---
+
+With the toboggan login problems resolved, you set off toward the airport. While travel by toboggan might be easy, it's certainly not safe: there's very minimal steering and the area is covered in trees. You'll need to see which angles will take you near the fewest trees.
+
+Due to the local geology, trees in this area only grow on exact integer coordinates in a grid. You make a map (your puzzle input) of the open squares (.) and trees (#) you can see. For example:
+
+..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#
+
+These aren't the only trees, though; due to something you read about once involving arboreal genetics and biome stability, the same pattern repeats to the right many times:
+
+..##.........##.........##.........##.........##.........##.......  --->
+#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+.#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+.#...##..#..#...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+..#.##.......#.##.......#.##.......#.##.......#.##.......#.##.....  --->
+.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+.#........#.#........#.#........#.#........#.#........#.#........#
+#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...
+#...##....##...##....##...##....##...##....##...##....##...##....#
+.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+
+You start on the open square (.) in the top-left corner and need to reach the bottom (below the bottom-most row on your map).
+
+The toboggan can only follow a few specific slopes (you opted for a cheaper model that prefers rational numbers);
+* start by counting all the trees you would encounter for the slope right 3, down 1:
+
+From your starting position at the top-left, check the position that is right 3 and down 1.
+* Then, check the position that is right 3 and down 1 from there, and so on until you go past the bottom of the map.
+
+The locations you'd check in the above example are marked here with O where there was an open square and X where there was a tree:
+
+..##.........##.........##.........##.........##.........##.......  --->
+#..O#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+.#....X..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+..#.#...#O#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+.#...##..#..X...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+..#.##.......#.X#.......#.##.......#.##.......#.##.......#.##.....  --->
+.#.#.#....#.#.#.#.O..#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+.#........#.#........X.#........#.#........#.#........#.#........#
+#.##...#...#.##...#...#.X#...#...#.##...#...#.##...#...#.##...#...
+#...##....##...##....##...#X....##...##....##...##....##...##....#
+.#..#...#.#.#..#...#.#.#..#...X.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+
+In this example, traversing the map using this slope would cause you to encounter 7 trees.
+
+Starting at the top-left corner of your map and following a slope of right 3 and down 1, how many trees would you encounter?
+
+*
+* --- Part Two ---
+
+Time to check the rest of the slopes - you need to minimize the probability of a sudden arboreal stop, after all.
+
+Determine the number of trees you would encounter if, for each of the following slopes, you start at the top-left corner and traverse the map all the way to the bottom:
+
+    Right 1, down 1.
+    Right 3, down 1. (This is the slope you already checked.)
+    Right 5, down 1.
+    Right 7, down 1.
+    Right 1, down 2.
+
+In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s) respectively; multiplied together, these produce the answer 336.
+
+What do you get if you multiply together the number of trees encountered on each of the listed slopes?
+
+ */
+class Day3(input: List<CharArray>) {
+    private val biome = Biome(input)
+
+    fun part1(): Int {
+        return getPathToBottom(Location(0, 0), listOf(), 1, 3).second.count { it == '#' }
+    }
+
+    fun part2(): Long {
+        return listOf(
+            Pair(1, 1),
+            Pair(1, 3),
+            Pair(1, 5),
+            Pair(1, 7),
+            Pair(2, 1)
+        ).map {
+            getPathToBottom(Location(0, 0), listOf(), it.first, it.second).second.count { c -> c == '#' }.toLong()
+        }.reduce(Long::times)
+    }
+
+    private fun getPathToBottom(
+        current: Location,
+        soFar: List<Char>,
+        xMove: Int,
+        yMove: Int
+    ): Pair<Location, List<Char>> {
+        val nextLocation = current.copy(x = current.x + xMove, y = current.y + yMove)
+        val nextChar = biome.getCharAt(nextLocation)
+        return if (nextChar == null) {
+            // base case!
+            Pair(nextLocation, soFar)
+        } else {
+            getPathToBottom(nextLocation, soFar + nextChar, xMove, yMove)
+        }
+    }
+
+    data class Location(val x: Int, val y: Int)
+    class Biome(basePattern: List<CharArray>) {
+
+        /*  x down
+         *  y right
+         */
+        private var currentPattern: List<CharArray> = basePattern
+
+        private fun expandPattern() {
+            currentPattern = currentPattern.map { it + it }
+        }
+
+        fun getCharAt(location: Location): Char? {
+            return when (val row = currentPattern.getOrNull(location.x)) {
+                null -> {
+                    // bottom! success!
+                    null
+                }
+                else -> {
+                    when (val column = row.getOrNull(location.y)) {
+                        null -> {
+                            expandPattern()
+                            currentPattern[location.x][location.y]
+                        }
+                        else -> column
+                    }
+                }
+            }
+        }
     }
 }
--- src/test/kotlin/Day3Test.kt
+++ src/test/kotlin/Day3Test.kt
@@ -1,11 +1,27 @@
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
+import java.nio.file.Paths
+import kotlin.test.assertEquals
 
 internal class Day3Test {
+    val input = Paths.get("src/main/resources/day3Input").toFile().readLines(Charsets.UTF_8).map { it.toCharArray() }
 
     @Test
-    fun doIt() {
-        Day3().doIt()
+    fun miniTest(){
+        val mininput =
+            """
+                ..#......###....#...##..#.#....
+                .#.#.....#.##.....###...##...##
+                ..#.#..#...........#.#..#......
+            """.trimIndent().split("\n").map { it.toCharArray() }
+        assertEquals( 1, Day3(mininput).part1())
+    }
+
+    @Test
+    fun part1() {
+        println(Day3(input).part1())
+    }
+    @Test
+    fun part2() {
+        println(Day3(input).part2())
     }
 }
